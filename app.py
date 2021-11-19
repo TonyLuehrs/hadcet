bb9094e26a452d5b8051a6a9fcc1d2bbf9b7dd09
TonyLuehrs <85131192+TonyLuehrs@users.noreply.github.com>
2021-11-19 16:19:53 -0600

    Update app.py
    added 4th section with 'about' info, fixed some layout issues to follow the dbc.Row -> dbc.Col convention

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from python.plot import Plot
 theme = dbc.themes.LUX
 css = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
 
-app = dash.Dash(name='__name__', external_stylesheets=[theme, css])
+app = dash.Dash(name='name', external_stylesheets=[theme, css])
 server = app.server
 app.title = 'HADCET Temperature Data'
 
@@ -42,6 +42,9 @@ plot = Plot(hadcet.df, hadcet.ave_df)
 # initial values we'll use for the start of the dashboard
 day0, month0, year0 = 1, 1, 2020
 temp0 = 5
+
+# easier to reference this unicode string than put it directly in a string
+degree_sign = '\u00b0'
 
 # initial graphs/values we'll use for the start of the dashboard
 start_lineplot = plot.get_lineplot(year0, day0)
@@ -68,7 +71,8 @@ window_size_input = dcc.Input(id='window-size-input', min=1, max=35, step=2, val
 month_input = dcc.Dropdown(id='month-input', placeholder='month', value='jan',
                            options=[{'label': long_months[i], 'value': short_months[i]} for i in range(12)])
 day_input = dcc.Input(id='day-input', placeholder='day', value=1, min=1, max=31, step=1, type='number')
-temp_input = dcc.Input(id='temp-input', placeholder='temp', value=0, min=-20, max=40, step=1, type='number')
+temp_input = dcc.Input(id='temp-input', placeholder='temp', value=0, min=-20, max=40, step=1, type='number',
+                       style={'display': 'inline-block'})
 
 # Inputs for Part 3
 multi_year_input = dcc.Dropdown(id='multi-year-input', value=['2020'], multi=True,
@@ -86,12 +90,14 @@ month_span_input = dcc.RangeSlider(id='month-span-input', min=1, max=12, step=No
 # PART 1 - ALLTIME VS. SELECTED YEAR
 layout_part1 = html.Div([
     dbc.Row([
-        html.Div('Comparing all-time daily average temperatures to a selected year',
-                 style={'font-weight': 'bold', 'font-size': '30px'}),
-        html.Br(), html.Br(), html.Br(), html.Br(),
+        dbc.Col([
+            html.Div('Compare Selected Year to All-time Daily Average',
+                     style={'font-weight': 'bold', 'font-size': '32px'})])]),
+    html.Br(), html.Br(),
+    dbc.Row([
         dbc.Col(children=[
             html.Br(),
-            html.Div('Select a year', style={'font-weight': 'bold', 'font-size': '16px'}),
+            html.Div('Select year', style={'font-weight': 'bold', 'font-size': '16px'}),
             year_input,
             html.Br(), html.Br(), html.Br(),
             html.Div('Select size of rolling average window', style={'font-weight': 'bold', 'font-size': '16px'}),
@@ -105,23 +111,26 @@ layout_part1 = html.Div([
     html.Br(), html.Br(),
     html.Hr()])
 
+
 # PART 2 ROW 1 - HISTORICAL DATA FOR A SINGLE DATE - INPUTS, HISTOGRAM, PROBABILITY STATEMENT
 layout_part2_row1 = html.Div([
-    html.Div('Examining Single Dates in History',
-             style={'font-weight': 'bold', 'font-size': '30px'}),
-    html.Br(), html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.Div('Examine Single Dates in History',
+                     style={'font-weight': 'bold', 'font-size': '32px'})])]),
+    html.Br(),
     dbc.Row([
         dbc.Col(children=[
             html.Br(),
-            html.Div('Select a month', style={'font-weight': 'bold', 'font-size': '16px'}),
+            html.Div('Select month', style={'font-weight': 'bold', 'font-size': '16px'}),
             month_input,
             html.Br(),
-            html.Div('Select a day', style={'font-weight': 'bold', 'font-size': '16px'}),
+            html.Div('Select day', style={'font-weight': 'bold', 'font-size': '16px'}),
             day_input,
             html.Br(), html.Br(),
-            html.Div('Select a temperature', style={'font-weight': 'bold', 'font-size': '16px'}),
+            html.Div('Select temperature', style={'font-weight': 'bold', 'font-size': '16px'}),
             temp_input,
-            html.Div('(degrees Celsius)', style={'font-size': '10px'}),
+            html.Div(f'{degree_sign}C'.rjust(5), style={'font-size': '16px', 'display': 'inline-block'}),
             html.Br(), html.Br()],
             width={'offset': 0, 'size': 2}, align='center',
             style={'border': '4px #073763 solid', 'border-radius': '4px', 'height': '290px'}),
@@ -130,17 +139,19 @@ layout_part2_row1 = html.Div([
             html.Div(id='day-perc-geq',
                      children=[html.P(f'← \n'),  # , style={'font-size': '24px', 'align': 'center'}),
                                html.P(f'{start_day_perc_geq}% of days on record with the date of'
-                                      f'{long_months[0]} {1} had a mean temperature at least as high'
-                                      f'as {0} degrees Celius.')],
+                                      f'{long_months[0]} {1} had a mean temperature of at least'
+                                      f'{0}C.')],
                      style={'font-size': '18px', 'border': '4px #073763 solid', 'border-radius': '4px',
                             'height': '290px', 'width': '190px', 'padding': '10px'})],
                 width={'offset': 0, 'size': 2},
                 align='center')])])
 
+
 # colors for the three tables in layout_part2_row2
 recent_table_color = '#d4e4da'  # greenish
 record_min_table_color = '#cacde4'  # bluish
 record_max_table_color = '#e4d4d4'  # reddish
+
 
 # PART 2 ROW 2 - HISTORICAL DATA FOR A SINGLE DATE - RECENT 5, RECORD LOWS, RECORD HIGHS TABLES
 layout_part2_row2 = html.Div([
@@ -180,8 +191,10 @@ layout_part2_row2 = html.Div([
 
 # PART 3 - COMPARING YEARS
 layout_part3 = html.Div([
-    html.Div('Comparing Multiple Years to All-Time Average',
-             style={'font-weight': 'bold', 'font-size': '30px'}),
+    dbc.Row([
+        dbc.Col([
+            html.Div('Compare Multiple Years to All-Time Daily Average',
+                     style={'font-weight': 'bold', 'font-size': '32px'})])]),
     html.Br(), html.Br(),
     dbc.Row(children=[
         dbc.Col(children=[
@@ -214,19 +227,51 @@ layout_part3 = html.Div([
                                            'background-color': plot.light_colors[i]} for i in range(5)]),
             width={'offset': 1, 'size': 2})],
             justify='left'),
+    html.Br(), html.Br(), html.Br(), html.Hr()])
+
+
+
+# PART 4 - "ABOUT" SECTION
+layout_part4 = html.Div([
+    html.Div('Links', style={'font-weight': 'bold', 'font-size': '24px', 'margin-left': '10px'}),
+    html.Div("The HadCET data is hosted by the United Kingdom's Meteorological Office "
+             "and can be found here:", style={'margin-left': '20px'}),
+    html.A("https://www.metoffice.gov.uk/hadobs/hadcet/", href='https://www.metoffice.gov.uk/hadobs/hadcet/',
+           target="_blank", style={'margin-left': '30px', "text-decoration": "underline"}),
     html.Br(), html.Br(),
-    # dbc.Row(children=[
-    #     dbc.Col(children=[
-    #         month_span_input],
-    #         width={'offset': 3, 'size': 7})#, align='right')
-    # ]),
-    html.Br(), html.Br(), html.Br()])
+    html.Div('This dashboard was created by Tony Luehrs. You can find all of the code '
+             'used to generate this dashboard at my github:', style={'margin-left': '20px'}),
+    html.A("https://www.github.com/TonyLuehrs/hadcet", href='https://www.github.com/TonyLuehrs/hadcet',
+           target="_blank", style={'margin-left': '30px', "text-decoration": "underline"}),
+    html.Br(), html.Br(),
+
+    html.Div('Dataset History and Citation', style={'font-weight': 'bold', 'font-size': '24px', 'margin-left': '10px'}),
+    html.Div('Monthly average temperature began being record in 1659.', style={'margin-left': '20px'}),
+    html.Div('Daily average temperatures began being recorded on Jan. 1, 1772.', style={'margin-left': '20px'}),
+    html.Div('Daily minimum and maximum temperatures began being recorded in 1878.', style={'margin-left': '20px'}),
+    html.Br(),
+    html.Div('It is important to note that the data assembled in this dataset came from a variety of sources,'
+             ' a variety of locations within central England, and in a variety of indoor and outdoor environments.'
+             ' For full details on methodologies, see the following paper by Parker, Legg, and Foland:',
+             style={'margin-left': '20px'}),
+    html.Br(),
+    html.A("https://www.metoffice.gov.uk/hadobs/hadcet/Parker_etalIJOC1992_dailyCET.pdf",
+           href='https://www.metoffice.gov.uk/hadobs/hadcet/Parker_etalIJOC1992_dailyCET.pdf',
+           target="_blank", style={'margin-left': '30px', "text-decoration": "underline"}),
+    html.Div('Parker, D.E., T.P. Legg, and C.K. Folland. 1992. A new daily Central England',
+             style={'margin-left': '35px'}),
+    html.Div('Temperature Series, 1772-1991. Int. J. Clim., Vol 12, pp 317-342',
+             style={'margin-left': '35px'}),
+    html.Br(), html.Br(), html.Hr()
+
+])
 
 
 
 #################
 ### Callbacks ###
 #################
+
 
 
 # PART 1 CALLBACK - UPDATE SMOOTHED LINEPLOT OF ONE YEAR'S TEMPERATURES
@@ -237,6 +282,7 @@ layout_part3 = html.Div([
         Input(component_id='window-size-input', component_property='value')])
 def update_part1(year, window_size):
     return plot.get_lineplot(year, window_size)
+
 
 
 # PART 2 CALLBACK 1 - UPDATE WHICH DAYS CAN BE SELECTED BASED ON THE MONTH SELECTED
@@ -267,6 +313,7 @@ def update_available_days(month, day):
 #     return hadcet.ave_df[hadcet.ave_df['month'] == month_dict[month]]['day'].max()
 
 
+
 # PART 2 CALLBACK 2 - UPDATE SINGLE DATE IN HISTORY, TEMP
 @app.callback(
     output=[
@@ -289,14 +336,15 @@ def update_part2(month, day, temp):
     day_perc_geq = [html.P(f'← \n'),
                     html.P(f'{hadcet.get_perc_geq(short_months.index(month) + 1, day, temp)}% of days on '
                            f'record with the date of {long_months[short_months.index(month)]} {day}'
-                           f' had a mean temperature at least as high as {temp}')]
-    day_perc_geq[1].children += ' degree Celsius.' if temp in [1, -1] else ' degrees Celsius.'
+                           f' had a mean temperature of at least {temp}{degree_sign}C.')]
     day_records_title_min = f'Record lows on {long_months[short_months.index(month)]} {day}:'
     day_records_title_max = f'Record highs on {long_months[short_months.index(month)]} {day}:'
     day_records_min, day_records_max = hadcet.get_day_records(short_months.index(month) + 1, day)
     return day_hist, day_recent_title, day_recent_5yr.to_dict('records'), day_perc_geq, \
            day_records_title_min, day_records_title_max, \
            day_records_min.to_dict('records'), day_records_max.to_dict('records')
+
+
 
 # PART 3 CALLBACK 1 - MAX OF 5 YEARS SELECTED IN MULTI-YEAR INPUT
 @app.callback(
@@ -308,6 +356,8 @@ def update_part3_options(multi_year):
         return [{'label': str(yr), 'value': str(yr)} for yr in multi_year]
     else:
         return [{'label': str(yr), 'value': str(yr)} for yr in range(2020, 1771, -1) if yr not in multi_year]
+
+
 
 # PART 3 CALLBACK 2 - UPDATE MULTI-SELECT YEAR TABLE, GRAPH
 @app.callback(
@@ -347,7 +397,8 @@ app.layout = dbc.Container(children=[
     layout_part1, html.Br(), html.Br(),
     layout_part2_row1, html.Br(),
     layout_part2_row2, html.Br(), html.Br(),
-    layout_part3, html.Br(), html.Br()
+    layout_part3, html.Br(), html.Br(),
+    layout_part4, html.Br(), html.Br()
 ])
 
 if __name__ == '__main__':
